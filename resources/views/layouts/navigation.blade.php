f41bd1e273ac45519bbdb8c4b2a2bdd3b31339f6
MitzIgnacio <150250954+MitzIgnacio@users.noreply.github.com>
2025-09-09 01:07:45 +0800

    some notary updates
    fard fard faaaaaaard

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -18,7 +18,7 @@
                 </div>
             </div>
 
-            <!-- Settings Dropdown for Authenticated Users Only -->
+            <!-- settings dropdown for auth users-->
             @if(Auth::check())
             <div class="hidden sm:flex sm:items-center sm:ms-6">
                 <x-dropdown align="right" width="48">
@@ -39,7 +39,7 @@
                             {{ __('Profile') }}
                         </x-dropdown-link>
 
-                        <!-- Authentication -->
+                        <!-- Auth -->
                         <form method="POST" action="{{ route('logout') }}">
                             @csrf
                             <x-dropdown-link :href="route('logout')"
@@ -53,7 +53,7 @@
             </div>
             @endif
 
-            <!-- Hamburger -->
+            <!-- Hamburger mmm burger -->
             <div class="-me-2 flex items-center sm:hidden">
                 <button @click="open = ! open" class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 dark:text-gray-500 hover:text-gray-500 dark:hover:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-900 focus:outline-none focus:bg-gray-100 dark:focus:bg-gray-900 focus:text-gray-500 dark:focus:text-gray-400 transition duration-150 ease-in-out">
                     <svg class="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
@@ -65,7 +65,7 @@
         </div>
     </div>
 
-    <!-- Responsive Navigation Menu -->
+    <!-- responsive navigation menu -->
     <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden">
         <div class="pt-2 pb-3 space-y-1">
             <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
@@ -73,7 +73,7 @@
             </x-responsive-nav-link>
         </div>
 
-        <!-- Responsive Settings Options for Authenticated Users Only -->
+        <!-- responsive setting options for auth. users -->
         @if(Auth::check())
         <div class="pt-4 pb-1 border-t border-gray-200 dark:border-gray-600">
             <div class="px-4">
@@ -86,7 +86,7 @@
                     {{ __('Profile') }}
                 </x-responsive-nav-link>
 
-                <!-- Authentication -->
+                <!-- Auth -->
                 <form method="POST" action="{{ route('logout') }}">
                     @csrf
                     <x-responsive-nav-link :href="route('logout')"
