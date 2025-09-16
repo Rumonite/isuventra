6ed8c29d989173ea4b20473c2d317cc58c45996a
MitzIgnacio <150250954+MitzIgnacio@users.noreply.github.com>
2025-09-16 14:23:53 +0800

    Update EventFactory.php
    Better Factory for seeding

--- database/factories/EventFactory.php
+++ database/factories/EventFactory.php
@@ -9,10 +9,29 @@ class EventFactory extends Factory
     public function definition(): array
     {
         return [
-            'title'       => $this->faker->sentence(3),
-            'description' => $this->faker->paragraph(),
-            'location'    => $this->faker->city(),
-            'date'  => $this->faker->dateTimeBetween('-1 month', '+6 months'),
+            'title'       => $this->faker->randomElement([
+                'Tech Conference',
+                'Sports Meet',
+                'Cultural Festival',
+                'Hackathon',
+                'Leadership Seminar',
+                'Orientation Program',
+                'Music Concert',
+                'Science Fair',
+            ]) . ' ' . $this->faker->year(),
+
+            'description' => $this->faker->paragraphs(3, true),
+
+            'location'    => $this->faker->randomElement([
+                'Main Auditorium',
+                'Campus Gymnasium',
+                'Tech Park Hall',
+                'Student Center',
+                'Virtual (Zoom)',
+                $this->faker->city() . ' Convention Center',
+            ]),
+
+            'date'        => $this->faker->dateTimeBetween('-1 month', '+6 months'),
         ];
     }
 }
